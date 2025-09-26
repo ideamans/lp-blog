b06d47568c2b8b4d2e96fda9843f86db0b77095f
Kunihiko Miyanaga <miyanaga@ideamans.com>
2025-09-26 21:43:29 +0900

    GAタグを変更

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -101,7 +101,7 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-YQBLSY0PKS');
+gtag('config', 'G-EYZJ8VYLJT');
 `
     ],
     [
